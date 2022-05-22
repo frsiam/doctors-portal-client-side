a8390a31c90df61bbb418d177016473c7a232db4
frsiam <siam.frs@gmail.com>
2022-05-23 00:35:54 +0600

    load my appointment by query and show into a table

--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyAppointments = () => {
+    const [user] = useAuthState(auth)
+    const [appointments, setAppointments] = useState([]);
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:4000/booking?patient=${user.email}`)
+                .then(res => res.json())
+                .then(data => setAppointments(data))
+        }
+    }, [user])
     return (
         <div>
-            <h1>Here is my appointment</h1>
+            <h1>Here is my appointment: {appointments.length}</h1>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Treatment Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            appointments.map((a, index) => <tr key={index}>
+                                <th>{index + 1}</th>
+                                <td>{a.patientName}</td>
+                                <td>{a.date}</td>
+                                <td>{a.slot}</td>
+                                <td>{a.treatmentName}</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
